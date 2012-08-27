04892f13df7d216531aeb5248696b8a06027c97d
z7psvfsati <z7psvfsati@no-spam.ws>
2012-08-27 06:04:08 -0700

    Cwiczenie numer 1 - Nazewnictwo. Zadanie: nazwij prawidłowo wszystkie elementy programu.

---
+++ Cwiczenie #1/FORMS/unit1.pas
@@ -0,0 +1,36 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
